d70441ec9f4cf38e427051ce95a0e20da5f284c1
Sergey Voytovich <s.voytovich@outlook.com>
2026-09-17 12:24:59 +0200

    declare chat service

---
+++ src/SupportAssistant.Application/Chats/IChatService.cs
@@ -0,0 +1,6 @@
+﻿namespace SupportAssistant.Application.Chats;
+
+public interface IChatService
+{
+    Task<ChatResult> Handleasync(ChatCommand command, CancellationToken cancellationToken = default);
+}
