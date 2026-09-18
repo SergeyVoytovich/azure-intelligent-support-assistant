f9ede5b51a77825fe9359db89338285cea42851d
Sergey Voytovich <s.voytovich@outlook.com>
2026-09-18 09:43:07 +0200

    declare text chunker

---
+++ src/SupportAssistant.Application/Knowladge/ITextChunker.cs
@@ -0,0 +1,6 @@
+﻿namespace SupportAssistant.Application.Knowladge;
+
+public interface ITextChunker
+{
+    IReadOnlyCollection<TextChunk> Chunk(string text, string source, int? page = null);
+}
---
+++ src/SupportAssistant.Application/Knowladge/TextChunk.cs
@@ -0,0 +1,9 @@
+﻿namespace SupportAssistant.Application.Knowladge;
+
+public record TextChunk
+{
+    public string Content { get;set; }
+    public int Index { get; set; }
+    public string Source { get; set; }
+    public int? Page { get; set; }
+}
